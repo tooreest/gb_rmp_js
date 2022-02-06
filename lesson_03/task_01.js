3cc74be642b628f78c80a84db3c119c8729a4963
Roman Papko <tooreest@gmail.com>
2022-02-06 13:15:52 +0300

    GB. JS. Lesson 03.

---
+++ lesson_03/task_01.js
@@ -0,0 +1,31 @@
+/*
+С помощью цикла while вывести все простые числа в промежутке от 0 до 100
+*/
+
+
+function checkPrimeNumber(num) {
+    if (num % 2 == 0) { return false; }
+    else {
+        let to = num / 2;
+        for (let i = 3; i < to; i += 2) {
+            if (num % i == 0) { return false; }
+            to = num / i;
+        }
+        return true;
+    }
+}
+
+
+let n = 100;
+
+document.writeln('<h3>Задание 1.</h3>');
+document.writeln('Простые числа от 1 до ', n, ' - <br>');
+
+let i = 0;
+while (i <= n) {
+    if (checkPrimeNumber(i)) {
+        document.writeln(i, ',');
+        console.log(i);
+    }
+    i++;
+}
---
+++ lesson_03/task_03.js
@@ -0,0 +1,10 @@
+/*
+Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
+
+for(…){// здесь пусто}
+*/
+
+document.writeln('<h3><br>Задание 3.<br></h3>');
+document.writeln('for (i = 1; i <= 10; document.writeln(i), i++) { }; ==> ');
+for (i = 1; i <= 10; document.writeln(i, ','), i++) { };
+for (i = 1; i <= 10; console.log(i), i++) { };
